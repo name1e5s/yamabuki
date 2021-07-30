1555d707495b7b80a134d3832c502b0f9eb3c9bb
name1e5s <name1e5s@qq.com>
2021-07-30 12:41:51 +0800

    about wtmp

---
+++ login_count.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+last | awk '{print $1}' | grep -v 'wtmp' | grep -v '^$'  | sort -n | uniq -c | awk '{print $2":"$1}'
---
+++ online_time.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ac -p | grep -v 'total' | awk '{print $1":"$2}'
